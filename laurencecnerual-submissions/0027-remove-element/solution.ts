848621b372ac25f2c0586076aeae175c96c54372
Laurence Agina <47619680+laurencecnerual@users.noreply.github.com>
2025-02-22 04:46:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55.3 MB (5.01%)

---
+++ laurencecnerual-submissions/0027-remove-element/solution.ts
@@ -0,0 +1,7 @@
+function removeElement(nums: number[], val: number): number {
+    while (nums.indexOf(val) !== -1) {
+        nums.splice(nums.indexOf(val), 1);
+    }
+
+    return nums.length;
+};
